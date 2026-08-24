289aabcbf62a04f01281f949a832fd3819c1ecbd
kalana20000 <kalana@rumex.lk>
2026-08-24 21:39:10 +0530

    add node.js module file

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [
+      {
+        name   : "config-server",
+        script: "java",
+        args : "-jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar",
+        logs : "./logs/config-server.log"
+      },
+
+     {
+        name   : "service-registry",
+        script: "java",
+        args : "-jar ./service-registry/target/service-registry-0.0.1-SNAPSHOT.jar",
+        logs : "./logs/service-registry.log"
+     },
+
+     {
+        name   : "api-gateway",
+        script: "java",
+        args : "-jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
+        logs : "./logs/api-gateway.log"
+     }
+
+  ]
+}
